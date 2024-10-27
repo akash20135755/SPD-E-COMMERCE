2fb834c675f961da7d63a5ffdba5ee0fef806ede
Saran-067 <153472653+Saran-067@users.noreply.github.com>
2024-10-27 12:43:45 +0530

    Update products.js

--- models/products.js
+++ models/products.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    category:{type: String,required: true},
     price: { type: Number, required: true },
     description: { type: String },
     stock: { type: Number, required: true },
